b98db023a2a13430ade741ea99c46bcf97fa40ed
Hugo Averty <hugoaverty@gmail.com>
2024-04-30 14:17:47 +0200

    Adding roof appear and disappear

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,20 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
+    WA.room.onEnterLayer("roofZone").subscribe(() => {
+        WA.room.hideLayer("Roof/Roof1");
+        WA.room.hideLayer("Roof/Roof2");
+        WA.room.hideLayer("Roof/Roof3");
+        WA.room.hideLayer("Roof/Roof4");
+    });
+
+    WA.room.onLeaveLayer("roofZone").subscribe(() => {
+        WA.room.showLayer("Roof/Roof1");
+        WA.room.showLayer("Roof/Roof2");
+        WA.room.showLayer("Roof/Roof3");
+        WA.room.showLayer("Roof/Roof4");
+    });
+
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
